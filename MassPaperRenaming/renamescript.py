7e56ed13588ed2850bc16312a0e867062bd9c8e8
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2023-04-16 23:40:45 +0500

    Add files via upload

---
+++ MassPaperRenaming/renamescript.py
@@ -0,0 +1,34 @@
+import os
+from PyPDF2 import PdfReader as pdf_reader
+import shutil
+
+# specify the input and output folder paths
+input_folder_path = "C:\Users\Cr7th\Desktop\dr"
+output_folder_path = "C:\Users\Cr7th\Desktop\DronesNames"
+titles=[]
+# create the output folder if it doesn't exist
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
+
+# iterate over each file in the input folder
+for filename in os.listdir(input_folder_path):
+    # check if the file is a PDF
+    if filename.endswith(".pdf"):
+        file_path = os.path.join(input_folder_path, filename)
+
+        # open the PDF and extract the title
+        with open(file_path, 'rb') as file:
+            pdf = pdf_reader(file)
+            titles.append(pdf.metadata.title)
+            titles.append(",")
+            print(titles)
+        # # create a new filename based on the title
+        # if(title!=None):
+        #     new_filename = title.strip().replace(" ", "_") + ".pdf"
+        #     new_file_path = os.path.join(output_folder_path, new_filename)
+
+        #     # copy the file to the output folder with the new filename
+        #     shutil.copyfile(file_path, new_file_path)
+            
+        #     # print a message to indicate which file was renamed
+        #     print(f"{filename} -> {new_filename}")
